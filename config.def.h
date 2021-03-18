ad38c9194acb03c9aad50a6ae08c9f301a6f1e1d
Maki XD <maki@void.home>
2021-03-18 17:11:11 +1000

    > using nerd font

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "White Rabbit:pixelsize=12:antialias=true:autohint=true";
-static int borderpx = 2;
+static char *font = "Hack Nerd Font Mono:pixelsize=12:antialias=true:autohint=true";
+static int borderpx = 4;
 
 /*
  * What program is execed by st depends of these precedence rules:
@@ -27,7 +27,7 @@ char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
 static float cwscale = 1.0;
-static float chscale = 1.5;
+static float chscale = 1.0;
 
 /*
  * word delimiter string
